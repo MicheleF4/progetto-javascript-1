83f00437bb45492ccedac8b6566f5da28ae9d589
MicheleF4 <michelefano.learn@gmail.com>
2022-03-11 22:34:54 +0100

    implementazione componenti in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,43 @@
 "use strict";
 
 let box = document.querySelector(".box");
-let displayText = document.querySelector(".box__display-text");
 let displayScreen = document.querySelector(".box__display");
 
+let displayContainer = document.querySelector(".box__display-container");
+let buttonContainer1 = document.getElementById("buttonContainer-1");
+let buttonContainer2 = document.getElementById("buttonContainer-2");
+let boxCol2 = document.querySelector(".box__col--2");
+
+let displayText = document.createElement("h1");
+let buttonDecrease = document.createElement("button");
+let buttonDecrease2 = document.createElement("button");
+let buttonIncrease = document.createElement("button");
+let buttonReset = document.createElement("button");
+
+displayText.classList.add("box__display-text");
+buttonDecrease.classList.add("box__button", "decrease-button");
+buttonDecrease2.classList.add("box__button", "decrease-button", "u-hidden");
+buttonIncrease.classList.add("box__button", "increase-button");
+buttonReset.classList.add("box__reset-btn", "u-button-universal");
+
+buttonDecrease.textContent = "-";
+buttonDecrease2.textContent = "-";
+buttonIncrease.textContent = "+";
+buttonReset.textContent = "RESET";
+
+buttonContainer1.append(buttonDecrease);
+buttonContainer2.append(buttonDecrease2);
+buttonContainer2.append(buttonIncrease);
+displayContainer.append(displayText);
+boxCol2.append(buttonReset);
+
+buttonDecrease.setAttribute("data-action", "decrease");
+buttonDecrease2.setAttribute("data-action", "decrease");
+buttonIncrease.setAttribute("data-action", "increase");
+buttonReset.setAttribute("data-action", "reset");
+
 let num = 0;
+displayText.textContent = num;
 
 box.addEventListener("click", function (e) {
   e = event.target.dataset.action;
